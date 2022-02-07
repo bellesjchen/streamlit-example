4f81f25e7f4d9a5abede3a1820b4b5e100374b53
bellesjchen <99162095+bellesjchen@users.noreply.github.com>
2022-02-07 17:34:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,7 @@ import streamlit as st
 
 st.markdown('Streamlit is **_really_ cool**.')
 st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+
+url = 'http://colormind.io/api/'
+r = requests.get(url)
+print("Status code:", r.status_code)
