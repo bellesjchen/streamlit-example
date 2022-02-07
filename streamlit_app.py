a093821c32bfd55b3a696732454592f4b52555d3
bellesjchen <99162095+bellesjchen@users.noreply.github.com>
2022-02-07 17:21:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,4 @@ import math
 import pandas as pd
 import streamlit as st
 
-df = pd.DataFrame(
-    np.random.randn(50, 20),
-    columns=('col %d' % i for i in range(20)))
-
-st.dataframe(df)  # Same as st.write(df)
 st.markdown('Streamlit is **_really_ cool**.')
