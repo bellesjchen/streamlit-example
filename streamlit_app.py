5a3bc9f7824bc97ea1d8d4543966e71ec3b9c892
bellesjchen <99162095+bellesjchen@users.noreply.github.com>
2022-02-07 18:40:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,22 +9,25 @@ import requests
 
 data = '{"model":"default"}'
 response = requests.post('http://colormind.io/api/', data=data)
-response.json()
+color_list = response.json()
+R = color_list['result'][0][0]
+G = color_list['result'][0][1]
+B = color_list['result'][1][1]
 
 st.markdown('Streamlit is **_really_ cool**.')
 st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
 
 col1, col2 ,col3= st.columns(3)
 with col1:
-     color1 = st.color_picker('pick the starting color', '#1aa3ff',key=1)
+     color1 = st.color_picker('pick the starting color', RRR(%d,%d,%d),key=1)
      st.write(f"you picked {color1}")
 with col2:
-     color2 = st.color_picker('pick the ending color', '#00ff00',key=2)
+     color2 = st.color_picker('pick the ending color', GGG(%d,%d,%d),key=2)
      st.write(f"you picked {color2}")
 with col3:
-     color3 = st.color_picker('pick the text color', '#ffffff',key=3)
+     color3 = st.color_picker('pick the text color', BBB(%d,%d,%d),key=3)
      st.write(f"you picked {color3}")
-text=st.text_input(" what you need to add")
+text=st.text_input(" the text you want to input")
 
 def example(color1, color2, color3, content):
      st.markdown(f'<p style="text-align:center;background-image: linear-gradient(to right,{color1}, {color2});color:{color3};font-size:24px;border-radius:2%;">{content}</p>', unsafe_allow_html=True)
