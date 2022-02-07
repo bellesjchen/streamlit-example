4f3fb9a52a31ea30f89a0f7bcb040cbb8ef103ea
bellesjchen <99162095+bellesjchen@users.noreply.github.com>
2022-02-07 17:39:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,15 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
-import requests
+import requests as req
+import json
 
 st.markdown('Streamlit is **_really_ cool**.')
 st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
 
-url = 'http://colormind.io/api/'
-r = requests.get(url)
-print("Status code:", r.status_code)
+
+data = '{"model":"default"}'
+response = requests.post('http://colormind.io/api/', data=data)
+response.json()
+
+{'result': [[71, 94, 97], [109, 152, 137], [183, 213, 193], [243, 239, 204], [240, 153, 122]]}
