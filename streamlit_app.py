e94b7501881d54455ec1cbfe2d4be07617de2caa
bellesjchen <99162095+bellesjchen@users.noreply.github.com>
2022-02-07 17:49:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,9 @@ import json
 st.markdown('Streamlit is **_really_ cool**.')
 st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
 
+{'result': ["hello","World"]}
 
 data = '{"model":"default"}'
 response = requests.post('http://colormind.io/api/', data=data)
 response.json()
 
-{'result': ["hello","World"]}
