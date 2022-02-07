800b61dafb613149af1cf937c624a06b3d72a5a4
bellesjchen <99162095+bellesjchen@users.noreply.github.com>
2022-02-07 17:36:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import requests
 
 st.markdown('Streamlit is **_really_ cool**.')
 st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
