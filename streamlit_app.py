16d2b815238adcb0cf920f9b91c314445e359d11
bellesjchen <99162095+bellesjchen@users.noreply.github.com>
2022-02-07 19:07:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,26 +6,29 @@ import streamlit as st
 import requests
 import json
 import requests
+from colormap import rgb2hex
 
 data = '{"model":"default"}'
 response = requests.post('http://colormind.io/api/', data=data)
 color_list = response.json()
 R = color_list['result'][0][0]
 G = color_list['result'][0][1]
-B = color_list['result'][1][1]
+B = color_list['result'][0][1]
+
+a='#1aa3ff'
 
 st.markdown('Streamlit is **_really_ cool**.')
 st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
 
 col1, col2 ,col3= st.columns(3)
 with col1:
-     color1 = st.color_picker('pick the starting color', 'rgb(3,5,7)',key=1)
+     color1 = st.color_picker('pick the starting color',a,key=1)
      st.write(f"you picked {color1}")
 with col2:
-     color2 = st.color_picker('pick the ending color', 'rgb(3,5,7)',key=2)
+     color2 = st.color_picker('pick the ending color', a,key=2)
      st.write(f"you picked {color2}")
 with col3:
-     color3 = st.color_picker('pick the text color', 'rgb(3,5,7)',key=3)
+     color3 = st.color_picker('pick the text color', a,key=3)
      st.write(f"you picked {color3}")
 text=st.text_input(" the text you want to input")
 
